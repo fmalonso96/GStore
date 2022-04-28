492d20be4db5c0b94e4bdc2b3a59deeea30162d8
Fernando Alonso <fmalonso96@gmail.com>
2022-04-28 01:32:48 -0300

    Now you can add a product as a favourite and it will be saved and remember due to Room Database implementation.

--- app/src/main/java/com/example/gstore/ui/favourites/view/FavouritesFragment.kt
+++ app/src/main/java/com/example/gstore/ui/favourites/view/FavouritesFragment.kt
@@ -6,14 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.gstore.R
+import com.example.gstore.data.database.DbRoom
 import com.example.gstore.databinding.FragmentFavouritesBinding
+import com.example.gstore.ui.favourites.FavouritesAdapter
+import com.example.gstore.ui.favourites.viewmodel.FavouritesViewModel
+import com.example.gstore.ui.products.viewmodel.ProductsViewModel
+import com.example.gstore.utils.ViewModelFactory
 
 class FavouritesFragment : Fragment() {
 
-    //private lateinit var viewModel: FavouritesViewModel
+    private lateinit var viewModel: FavouritesViewModel
     private lateinit var binding: FragmentFavouritesBinding
-    //private lateinit var dbRoom: DbRoom
+    private lateinit var dbRoom: DbRoom
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +28,28 @@ class FavouritesFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favourites, container, false)
+        dbRoom = DbRoom.getDatabase(requireContext())
+
+        setupViewModel()
+        setupUI()
+        setupObservers()
 
         return binding.root
+    }
+
+    private fun setupUI() {
+        binding.recyclerViewFavourites.layoutManager = GridLayoutManager(context, 2)
+    }
+
+    private fun setupViewModel() {
+        viewModel = ViewModelProvider(this, ViewModelFactory(dbRoom))[FavouritesViewModel::class.java]
+    }
+
+    private fun setupObservers() {
+        viewModel.currentFavouriteProducts.observe(this.viewLifecycleOwner) { favouriteList ->
+            val adapter = FavouritesAdapter(favouriteList)
+            binding.recyclerViewFavourites.adapter = adapter
+        }
     }
 
 }
